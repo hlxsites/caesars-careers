9f8bdee3231a61d08ff85daf48b617460e65e2aa
Tania Mathern <mathern@adobe.com>
2023-05-04 12:00:54 -0700

    Initial DOM is ready

--- careers/blocks/icons-layout/icons-layout.js
+++ careers/blocks/icons-layout/icons-layout.js
@@ -10,18 +10,35 @@ export default function decorate(block) {
   }
 
   const allIcons = initialIconsDiv.children[0];
-
   const iconNodes = [];
   let currentIconNode = null;
-  allIcons.forEach((iconLayoutElement) => {
+  [...allIcons.children].forEach((iconLayoutElement) => {
     // check if current element is an icon or not
-    // if icon, add previous icon to list, create new node
 
-    // if not icon, add content as is to current icon node
+    const hasIconSvg = iconLayoutElement.querySelector('svg');
+    if(hasIconSvg){
+      // new icon node, add previous icon to list, create new icon node
+      iconLayoutElement.classList.add('icon-layout-picture');
+      if(!!currentIconNode){
+        iconNodes.push(currentIconNode);
+      }
+
+      currentIconNode = document.createElement('div');
+      currentIconNode.classList.add('icon-layout-element');
+      currentIconNode.append(iconLayoutElement);
+    } else {
+      // if not icon, add content as is to currently built icon node
+      currentIconNode.append(iconLayoutElement);
+    }
+
   });
 
   // handle last icon node, if any, since we may go out of the loop before adding it
   if(!!currentIconNode){
     iconNodes.push(currentIconNode);
+    currentIconNode = null;
   }
+
+  initialIconsDiv.remove();
+  block.append(...iconNodes);
 }
