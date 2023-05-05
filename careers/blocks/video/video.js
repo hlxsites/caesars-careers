71a415d76187eb0760a18c1ad5f21bdb7b0d953c
Anthony Rumsey <arumsey@adobe.com>
2023-05-05 10:38:54 -0400

    Youtube block #20
    - support multiple videos on the same page
    - simplify DOM

--- careers/blocks/video/video.js
+++ careers/blocks/video/video.js
@@ -1,9 +1,5 @@
 import { createTag } from '../../scripts/scripts.js';
 
-/**
- * Keep track of all the YouTube players for each video on the page
- */
-const playerMap = {};
 const pendingPlayers = [];
 
 const videoTypeMap = Object.freeze({
@@ -45,18 +41,8 @@ const getYouTubeId = (href) => {
  * @param videoId The YouTube video id
  */
 const loadYouTubePlayer = (element, videoId) => {
-  const onPlayerReady = (event) => {
-    playerMap[videoId] = event.target;
-  };
-  // we have to create a new YT Player but then need to wait for its onReady event
-  // before assigning it to the player map
   // eslint-disable-next-line no-new
-  new window.YT.Player(element, {
-    videoId,
-    events: {
-      onReady: onPlayerReady,
-    },
-  });
+  new window.YT.Player(element, { videoId });
 };
 
 /**
@@ -69,59 +55,43 @@ const buildVideoPlayer = (href) => {
     return null;
   }
 
-  const videoContainer = createTag('div', { class: 'video-container' });
-  const videoContent = createTag('div', { class: 'video-content' });
+  const videoPlayer = createTag('div', { class: 'video-player' });
 
   // Create a YouTube compatible iFrame
   const videoId = getYouTubeId(href);
-  videoContent.dataset.ytid = videoId;
-  videoContent.innerHTML = `<div id="ytFrame-${videoId}"></div>`;
+  videoPlayer.dataset.ytid = videoId;
+  videoPlayer.innerHTML = `<div id="ytFrame-${videoId}"></div>`;
   if (!window.YT) {
-    pendingPlayers.push({ id: videoId, element: videoContent.firstElementChild});
+    pendingPlayers.push({ id: videoId, element: videoPlayer.firstElementChild});
   } else {
-    loadYouTubePlayer(videoContent.firstElementChild, videoId);
+    loadYouTubePlayer(videoPlayer.firstElementChild, videoId);
   }
 
   if (!window.onYouTubeIframeAPIReady) {
     // onYouTubeIframeAPIReady will load the video after the script is loaded
     window.onYouTubeIframeAPIReady = () => {
-      pendingPlayers.forEach(({videoId, element}) => loadYouTubePlayer(element, videoId));
+      pendingPlayers.forEach(({id, element}) => loadYouTubePlayer(element, id));
     }
   }
 
-  videoContainer.appendChild(videoContent);
-
-  return videoContainer;
+  return videoPlayer;
 };
 
 export default function decorate(block) {
-  // decorate picture container
+  // decorate static content
+  const videoPoster = block.querySelector(':scope > div');
+  videoPoster.classList.add('video-poster');
   const picture = block.querySelector('picture');
-  let altContainer;
   if (picture) {
-    // if there is text and picture
-    altContainer = picture.closest('div');
-    const videoImage = document.createElement('div');
-    videoImage.classList.add('video-image');
-    videoImage.append(picture);
-    altContainer.append(videoImage);
-  } else {
-    // text only
-    const altText = block.querySelector('h4');
-    altContainer = altText.closest('div');
+    picture.parentElement.classList.add('video-poster-image');
   }
-  const staticPlaceholder = altContainer.parentNode;
-  staticPlaceholder.classList.add('video-static-content');
 
   // decorate video link
   const videoLink = block.querySelector('a');
-  let videoHref;
   if (videoLink) {
-    videoHref = videoLink.href;
-    if (getVideoType(videoHref) !== 'external') {
-      const videoModal = buildVideoPlayer(videoHref);
-      block.append(videoModal);
-    }
-    videoLink.remove();
+    const videoHref = videoLink.href;
+    //const videoPlayer = buildVideoPlayer(videoHref);
+    //block.append(videoPlayer);
+    videoLink.parentElement.remove();
   }
 }
