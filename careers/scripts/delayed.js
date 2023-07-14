f65867b0edac8f267d0733abe0318e41dfffec74
Amol Anand <amol.anand@gmail.com>
2023-07-14 16:15:12 -0400

    loading global galaxy login script

--- careers/scripts/delayed.js
+++ careers/scripts/delayed.js
@@ -1,23 +1,9 @@
-// eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+// eslint-disable-next-line import/no-cycle
+import { loadScript } from './scripts.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-
-// loads additional delayed scripts
-const loadScript = (url, attrs) => {
-  const head = document.querySelector('head');
-  const script = document.createElement('script');
-  script.src = url;
-  if (attrs) {
-    // eslint-disable-next-line no-restricted-syntax, guard-for-in
-    for (const attr in attrs) {
-      script.setAttribute(attr, attrs[attr]);
-    }
-  }
-  head.append(script);
-  return script;
-};
 
 // add more delayed functionality here
 // for YouTube videos
--- careers/scripts/scripts.js
+++ careers/scripts/scripts.js
@@ -158,6 +158,21 @@ function buildSectionBackground(main) {
   });
 }
 
+// loads additional delayed scripts
+export function loadScript(url, attrs) {
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  script.src = url;
+  if (attrs) {
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const attr in attrs) {
+      script.setAttribute(attr, attrs[attr]);
+    }
+  }
+  head.append(script);
+  return script;
+}
+
 /**
  * Decorates the main element.
  * @param {Element} main The main element
