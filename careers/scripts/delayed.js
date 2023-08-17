6bfb9665262323aaa41e1966ac1e3bdcb5d6e904
Amol Anand <amol.anand@gmail.com>
2023-08-17 09:42:19 +0200

    Added GTM

--- careers/scripts/delayed.js
+++ careers/scripts/delayed.js
@@ -1,6 +1,4 @@
 import { sampleRUM } from './lib-franklin.js';
-// eslint-disable-next-line import/no-cycle
-import { loadScript } from './scripts.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
@@ -9,6 +7,33 @@ const adobeLaunchDev = 'https://assets.adobedtm.com/6a2d3120441b/542932800399/la
 const adobeLaunchStage = 'https://assets.adobedtm.com/6a2d3120441b/542932800399/launch-EN2a6f725268ea4ce5be8f4ae9f41c2ee7-staging.min.js';
 const adobeLaunchProd = 'https://assets.adobedtm.com/6a2d3120441b/542932800399/launch-ENc8ccf2ef24a24a7a93d1dfb757ad2f96.min.js';
 
+function loadScript(url, attrs, body) {
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  if (url) script.src = url;
+  if (attrs) {
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const attr in attrs) {
+      script.setAttribute(attr, attrs[attr]);
+    }
+  }
+  if (body) {
+    script.type = 'text/javascript';
+    script.text = body;
+  }
+
+  head.append(script);
+  return script;
+}
+
+// for YouTube videos
+if (document.querySelector('[data-ytid]')) {
+  loadScript('https://www.youtube.com/iframe_api', {
+    type: 'text/javascript',
+  });
+}
+
+// Add Adobe Launch
 if (window.location.host.startsWith('localhost')
     || window.location.host.endsWith('.page')) {
   loadScript(adobeLaunchDev);
@@ -19,10 +44,23 @@ if (window.location.host.startsWith('localhost')
   loadScript(adobeLaunchProd);
 }
 
-// add more delayed functionality here
-// for YouTube videos
-if (document.querySelector('[data-ytid]')) {
-  loadScript('https://www.youtube.com/iframe_api', {
-    type: 'text/javascript',
-  });
-}
+// Add Google Tag Manager
+const gtmIframe = document.createElement('iframe');
+gtmIframe.classList.add('gtm-iframe');
+gtmIframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-QRNS';
+const gtmEl = document.createElement('noscript');
+gtmEl.append(gtmIframe);
+document.body.prepend(gtmEl);
+
+const gtmCode = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-QRNS');`;
+
+loadScript('', {}, gtmCode);
+
+// Adobe Target
+window.targetGlobalSettings = {
+  bodyHidingEnabled: false,
+};
